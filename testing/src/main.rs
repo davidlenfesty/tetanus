54e5f58219b8f48dc8d31ef2fd51e1512cdaf491
David Lenfesty <lenfesty@ualberta.ca>
2019-02-21 19:42:14 -0700

    Light turns on!

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,24 +1,53 @@
+//! examples/init.rs
+
 #![deny(unsafe_code)]
 #![deny(warnings)]
 #![no_main]
 #![no_std]
 
 extern crate panic_semihosting;
+extern crate stm32f030_hal;
 
-//use cortex_m_semihosting::hprintln; 
-use rtfm::app;
 use cortex_m_semihosting::hprintln;
-
-// I believe if I pulled in the gpio as an external crate I wouldn't need these
-//use crate::gpio::GpioExt;
-//use crate::gpio::{Output, PushPull};
-//use crate::gpio::gpioa::PA5;
-//use embedded_hal::digital::OutputPin;
+use rtfm::app;
+use stm32f030_hal::gpio;
+use gpio::*;
+use gpioa::*;
 
 #[app(device = stm32f0::stm32f0x0)]
 const APP: () = {
+    static mut LED: gpioa::PA5<Output<PushPull>> = ();
+
     #[init]
-    fn init() {
-        hprintln!("Initializing!").unwrap();
+    fn init() -> init::LateResources {
+        static mut X: u32 = 0;
+
+        // Cortex-M peripherals
+        let _core: rtfm::Peripherals = core;
+        // Device specific peripherals
+        let _device: stm32f0::stm32f0x0::Peripherals = device;
+
+        let rcc = _device.RCC;
+        let gpioa = _device.GPIOA.split(&rcc.ahbenr);
+
+        let mut led = gpioa.pa5.into_output_push_pull();
+
+        // Safe access to local `static mut` variable
+        let _x: &'static mut u32 = X;
+
+        hprintln!("init").unwrap();
+
+        led.set_high();
+
+        init::LateResources {
+            LED: led,
+        }
+    }
+
+    #[idle]
+    fn idle() -> ! {
+        loop {
+            // do nothing for now
+        }
     }
 };
