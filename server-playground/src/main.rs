00b5e928a1b2e538dd7c3d1c6d72ec3c6924cdee
David Lenfesty <lenfesty@ualberta.ca>
2019-02-24 21:47:32 -0700

    Serving static files

--- server-playground/src/main.rs
+++ server-playground/src/main.rs
@@ -2,11 +2,19 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::serve::StaticFiles;
+
 #[get("/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello {}, age {}", name, age)
 }
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![hello]).launch();
+    //rocket::ignite().mount("/hello", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/public",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
+        .mount("/hello", routes![hello])
+        .launch();
+
 }
