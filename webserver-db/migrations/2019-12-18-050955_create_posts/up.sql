a595716628191ee69cfb184547dbb87a5f635fec
David Lenfesty <lenfesty@ualberta.ca>
2019-12-22 12:24:21 -0700

    Migrated webserver code to 'website' repo

--- webserver-db/migrations/2019-12-18-050955_create_posts/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE pages (
-    id SERIAL PRIMARY KEY,
-    parent_id INT NOT NULL,
-    name VARCHAR NOT NULL,
-    body TEXT NOT NULL,
-    is_folder BOOLEAN NOT NULL DEFAULT 'f',
-    published BOOLEAN NOT NULL DEFAULT 'f'
-)
